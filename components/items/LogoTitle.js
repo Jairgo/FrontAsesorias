50473a69674ff95513f948fefbe7f30187547aca
Jair Gómez Vásquez <jair.gomezv06@anahuac.mx>
2022-05-11 00:18:43 -0500

    Obtiene imagen del server, use context para compartir imagenes entre los componentes

--- components/items/LogoTitle.js
+++ components/items/LogoTitle.js
@@ -1,13 +1,15 @@
-import React from 'react';
-import {Image, StyleSheet} from 'react-native';
+import React, { useContext }  from 'react';
+import { Image, StyleSheet } from 'react-native';
 
-function LogoTitle( props ) {
+import { UserContext } from "../UserContext";
+
+function LogoTitle(props) {
+    const {user, setUser} = useContext(UserContext);
+
     return (
         <Image
-        style={styles.tinyLogo}
-        source={
-            require('../pictures/perfil.jpeg')
-        }
+            style={styles.tinyLogo}
+            source={{ uri: user ? user.link : 'https://www.jing.fm/clipimg/detail/375-3757880_my-account-profile-icon-transparent-white.png' }}
         />
     );
 }
