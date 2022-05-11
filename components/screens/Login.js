8f7c1717c2d672beeee3a2f1f3890eccdacfc7e1
MemoRT <guillermo.ramirezt15@anahuac.mx>
2022-05-10 23:12:52 -0500

    LoginStack Created

--- components/screens/Login.js
+++ components/screens/Login.js
@@ -73,11 +73,16 @@ export default function Login(props) {
           style={styles.textInput}
           secureTextEntry={true}
         />
+        <TouchableOpacity>
         <Text style={styles.forgotPassword}>Olvidaste tu contraseña?</Text>
+        </TouchableOpacity>
+        
       <Animatable.View animation="fadeInUp">
         <ButtonGradient changeView={() => props.changeView(true)}/>
         </Animatable.View>
+        <TouchableOpacity>
         <Text style={styles.forgotPassword}>Aún no tienes cuenta?</Text>
+        </TouchableOpacity>
         <StatusBar style="auto" />  
       </View>
     </View>
