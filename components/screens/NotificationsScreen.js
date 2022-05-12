0c127486e4732ca30093be75a82b7d5ce0c85bb4
Drell256 <jorge.dominguezj19@anahuac.mx>
2022-05-12 18:10:16 -0500

    Notificaciones sin enum

--- components/screens/NotificationsScreen.js
+++ components/screens/NotificationsScreen.js
@@ -1,11 +1,114 @@
-import { View, Text } from 'react-native';
+import react, { useState, useEffect } from "react";
+import { StyleSheet, View, Text, ScrollView } from 'react-native';
+import { endpoints } from "../constants/Backend";
+import NotificationsView from "./NotificationsView";
+import axios from 'axios';
 
-function NotificationsScreen() {
-    return (
-      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        <Text>Vista de notificaciones</Text>
-      </View>
-    );
+export default function NotificationsScreen() {
+
+  // const [notifications, setNotifications] = useState();
+  // const getNotifications = () => {
+
+  //   let endpoint = 'http://127.0.0.1/:8000/notificaciones/'
+
+  //   axios.get(endpoint).then((response) => {
+  //     console.log(JSON.stringify(response.data));
+  //     setNotifications(notifications)
+  //   }).catch(err => {
+  //     console.log(err);
+  //   }
+  //   );
+  // }
+
+  // useEffect(() => {
+  //   getNotifications();
+  // }, []);
+
+  const notifications = [
+    {
+      "id": 6,
+      "nombre": "Titulo de notificacion 6",
+      "descripcion": "descripcion de notificacion",
+      "fecha": "2022-05-12",
+      "estudiante": 1,
+      "created": "2022-05-12T17:57:13.272459-05:00",
+      "updated": "2022-05-12T17:57:13.272481-05:00"
+    },
+    {
+      "id": 5,
+      "nombre": "Titulo de notificacion 5",
+      "descripcion": "descripcion de notificacion",
+      "fecha": "2022-05-12",
+      "estudiante": 1,
+      "created": "2022-05-12T17:57:10.802024-05:00",
+      "updated": "2022-05-12T17:57:10.802046-05:00"
+    },
+    {
+      "id": 4,
+      "nombre": "Titulo de notificacion 4",
+      "descripcion": "descripcion de notificacion",
+      "fecha": "2022-05-12",
+      "estudiante": 1,
+      "created": "2022-05-12T17:57:08.283999-05:00",
+      "updated": "2022-05-12T17:57:08.284019-05:00"
+    },
+    {
+      "id": 3,
+      "nombre": "Titulo de notificacion 3",
+      "descripcion": "descripcion de notificacion",
+      "fecha": "2022-05-12",
+      "estudiante": 1,
+      "created": "2022-05-12T17:57:05.735620-05:00",
+      "updated": "2022-05-12T17:57:05.735641-05:00"
+    },
+    {
+      "id": 2,
+      "nombre": "Titulo de notificacion 2",
+      "descripcion": "descripcion de notificacion",
+      "fecha": "2022-05-12",
+      "estudiante": 1,
+      "created": "2022-05-12T17:57:03.382375-05:00",
+      "updated": "2022-05-12T17:57:03.382397-05:00"
+    },
+    {
+      "id": 1,
+      "nombre": "Titulo de notificacion 1",
+      "descripcion": "descripcion de notificacion",
+      "fecha": "2022-05-12",
+      "estudiante": 1,
+      "created": "2022-05-12T17:56:59.905500-05:00",
+      "updated": "2022-05-12T17:56:59.905522-05:00"
+    }
+  ];
+
+  console.log(notifications)
+
+  return (
+    <View style={styles.screen}>
+      <Text style={styles.title}>
+        Notificaciones
+      </Text>
+      <ScrollView>
+        {
+          notifications.map((notifications) => (
+            <NotificationsView
+              key={notifications.id}
+              nombre={notifications.nombre}
+              descripcion={notifications.descripcion}
+              fecha={notifications.fecha}
+            />
+          ))
+        }
+      </ScrollView>
+    </View>
+  );
 }
 
-export default NotificationsScreen;
+const styles = StyleSheet.create({
+  title: {
+    fontSize: 30,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    marginTop: 20,
+  }
+});
