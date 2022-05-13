809b09af6002017e49ca5530dc70107d62c2625f
JulioFabian <julio.fabianoj@anahuac.mx>
2022-05-12 23:00:43 -0500

    Cambios raros

--- components/constants/Strings.js
+++ components/constants/Strings.js
@@ -12,11 +12,8 @@ export default {
     removeButton: 'Eliminar',
     editButton: 'Editar',
     seleccionarHorario: 'Selecciona un horario',
-<<<<<<< HEAD:components/constants/Strings.js
     seleccionarMateria: 'Selecciona una materia',
     seleccionarFecha: 'Selecciona una fecha',
-=======
->>>>>>> jair:constants/Strings.js
     seleccionarAsesor: 'Selecciona un asesor',
     solicitarAsesoria: 'Solicitar asesoría',
 }
