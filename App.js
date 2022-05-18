ec8bca09b6e41725dcf5b2aa6b2d44e1bf9c51bd
Jair Gómez Vásquez <jair.gomezv06@anahuac.mx>
2022-05-17 21:13:02 -0500

    Remove unused file and imports"

--- App.js
+++ App.js
@@ -1,13 +1,7 @@
-import { SimpleGrid } from 'native-base';
-import React, { useState, useMemo, useEffect } from 'react';
+import React, { useState, useMemo } from 'react';
 import HeaderApp from './components/reusables/HeaderApp';
-// import HeaderApp from './components/reusables/HeaderApp';
-// import Login from './components/screens/Login';
-// import SingUp from './components/screens/SignUp';
-// import ForgotPassword from './components/screens/ForgotPassword';
 import LoginStack from './components/stacks/LoginStack'
 import { UserContext } from './components/UserContext';
-import { fakeUser } from "./components/Server";
 
 
 export default function App() {
@@ -16,14 +10,6 @@ export default function App() {
 
   const [isLoguedIn, setIsLoguedIn] = useState(false);
   const handlerLoguedIn = (val) => setIsLoguedIn(val);
-
-  // useEffect(() => {
-  //   async function fetchData() {
-  //     const actualUser = await fakeUser();
-  //     setUser(actualUser)
-  //   }
-  //   fetchData();
-  // }, [])
 
   return (
     <UserContext.Provider value={value}>
@@ -34,8 +20,6 @@ export default function App() {
           <LoginStack changeView={handlerLoguedIn} />
         )
       }
-      {/* // <SingUp />
-    // <ForgotPassword /> */}
     </UserContext.Provider>
   );
 }
